4f67de43ced192ad2a6e4cdc79e4697382902602
long-troi-lo-dat <hnbaolong.4work@gmail.com>
2024-06-03 04:27:23 +0700

    gitignore add .env + access control allow

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,6 +4,10 @@ const baseURL = process.env.REACT_APP_URL_API;
 const app = axios.create({
     baseURL,
     withCredentials: true,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    },
 })
 
 export default app
